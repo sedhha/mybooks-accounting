aec69a95ba0824164f6c2aea15b5d464aa58b860
sedhha <activity.schoolsh2@gmail.com>
2023-03-05 21:52:33 +0530

    Enabled Audiences

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import Footer from '@/components/Footer';
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<React.Fragment>
 			<Component {...pageProps} />
-				<Footer />
+			<Footer />
+			<Analytics />
 		</React.Fragment>
 	);
 }
